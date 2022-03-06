11665ef4bf4db23d8932815fa52702c12e804d5e
Sherwin <sherwin.sandoval026@gmail.com>
2022-03-06 14:51:38 +0800

    init

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+import React from "react";
+
+class Loading extends React.Component{
+    render(){
+        return (
+            <div className=" text-2xl font-bold">
+                <p>FROM HOME</p>
+            </div>
+        )
+    }
+}
+
+export default Loading
