6ca67036147ea02e6c868e1dcd685ff9f2fbf06f
Sherwin Sandoval <megaminions008@gmail.com>
2022-03-07 16:03:35 +0800

    Adjust breakpoint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,14 +40,15 @@ class Header extends Component{
                 <div>
                     <img src={logo} alt="logo" className="h-6 md:h-8 border border-black"/>
                 </div>
-                <MediaQuery maxWidth={425} onChange={handleMediaQueryChange}>
+                <MediaQuery maxWidth={426} onChange={handleMediaQueryChange}>
                     <button className="inline-block w-8 h-8 text-gray-600 p-1" onClick={this.showMenu}>
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     </button>
                     {this.state.showMenu ? (
-                    <nav className="  absolute space-y-4 top-16 left-0 z-20 flex flex-col font-semibold w-full bg-white shadow-md rounded-lg p-6 pt-0 font-mono">
+                    <nav className="  absolute space-y-4 top-16 left-0 z-20 flex flex-col font-semibold w-full bg-white 
+                    shadow-md rounded-lg p-6 pt-0 font-mono">
                         <a href="/" className=" hover:underline hover:text-indigo-600 active:text-indigo-600 text-gray-600">01. About Me</a>
                         <a href="/" className=" hover:underline hover:text-indigo-600 active:text-indigo-600 text-gray-600">02. Experience</a>
                         <a href="/" className=" hover:underline hover:text-indigo-600 active:text-indigo-600 text-gray-600">03. Projects</a>
@@ -59,7 +60,9 @@ class Header extends Component{
                 </MediaQuery>
                 
 
-                <nav className="  hidden absolute md:relative top-16 left-0 md:top-0 z-20 md:flex md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0 font-mono">
+                <nav className="  hidden absolute sm:relative top-16 left-0 sm:top-0 z-20 sm:flex sm:flex-row sm:space-x-6 
+                font-semibold w-full sm:w-auto bg-white shadow-md rounded-lg sm:rounded-none sm:shadow-none sm:bg-transparent p-6 pt-0 sm:p-0 
+                font-mono">
                     <a href="/" className=" hover:underline hover:text-indigo-600 active:text-indigo-600 text-gray-600">01. About Me</a>
                     <a href="/" className=" hover:underline hover:text-indigo-600 active:text-indigo-600 text-gray-600">02. Experience</a>
                     <a href="/" className=" hover:underline hover:text-indigo-600 active:text-indigo-600 text-gray-600">03. Projects</a>
