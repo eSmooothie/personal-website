171ebf4fe5289cdea18c18ad0c384d19f3c0f2f5
Sherwin Sandoval <sherwin.sandoval026@gmail.com>
2022-05-19 23:50:24 +0800

    Setup about me and work experience section.
    - Separate components per section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,19 +3,43 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {StyleRoot} from 'radium';
 import {animationFadeInLeft, animationFadeInUp, animationFadeIn} from '../constants/Animations';
+import { LoremIpsum } from "lorem-ipsum";
+
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4
+    }
+  });
+
+// list of companies I worked
+const list_companies = [
+    {
+        "company_name" : "FactSet Inc.",
+        "start_date" : "Feb 2022",
+        "end_date" : "May 2022",
+        "position" : "Software Engineer Intern"
+       
+    },
+    
+];
 
 function MyIntroduction(){
     return <StyleRoot>
-        <section className="  md:px-24 px-3 font-staatliches pt-1 min-h-screen overflow-x-hidden border border-white">
+        <section className=" flex justify-center font-staatliches min-h-screen overflow-x-hidden border border-white">
             {/* Intro */}
-            <div className=" relative top-9 xl:left-[22%] lg:left-[20%] left-10">
-                <p className=" text-green-400 text-3xl" style={animationFadeIn._1000ms}>Hi! I'm</p>
+            <div className=" relative sm:w-[45%] sm:px-0 px-5 border border-white">
+                <p className=" text-green-400 sm:text-3xl text-xl" style={animationFadeIn._1000ms}>Hi! I'm</p>
                 <p className=" text-gray-200 sm:text-9xl text-7xl xl:text-10xl" style={animationFadeIn._1000ms}>SHERWIN<br />SANDOVAL</p>
 
-                <article className=" text-gray-400 sm:text-xl xl:w-1/2 lg:w-4/6 md:w-5/6 w-9/12 text-justify" style={animationFadeIn._1000ms}>
-                I am ambitious and motivated. Constantly set goals for myself, 
-                so I have something to strive toward. I`m not comfortable with settling,
-                and I'm always looking for an opportunity to do better and achieve greatness.
+                <article className=" text-gray-400 sm:text-xl text-justify" style={animationFadeIn._1000ms}>
+                    I am ambitious and motivated. Constantly set goals for myself, 
+                    so I have something to strive toward. I`m not comfortable with settling,
+                    and I'm always looking for an opportunity to do better and achieve greatness.
                 </article>
             </div>
             {/* About Me */}
@@ -26,11 +50,17 @@ function MyIntroduction(){
 // Displays about section
 function AboutMyself(){
     return <StyleRoot>
-    <section id="about_me" className="  md:px-24 px-3 font-staatliches pt-1 min-h-screen overflow-x-hidden border border-white">
-        <div className=" relative top-9 xl:left-[22%] lg:left-[20%] left-10">
-            <article className=" text-gray-400 sm:text-xl xl:w-1/2 lg:w-4/6 md:w-5/6 w-9/12 text-justify" style={animationFadeIn._1000ms}>
-                About mE!
-            </article>
+    <section id="about_me" className=" flex justify-center items-center font-staatliches min-h-screen overflow-x-hidden border border-white">
+        <div className=" relative sm:w-[55%]">
+            <p className=" text-green-400 sm:text-3xl text-xl">About Me</p>
+            <div className=" grid grid-cols-3 border-white ">
+                <article className=" text-gray-400 sm:text-xl
+                text-justify col-span-2 px-4 border border-white" 
+                style={animationFadeIn._1000ms}>
+                    {lorem.generateParagraphs(2)}
+                </article>
+                <img src="" alt="my_photo" className=" border border-white col-span-1"/>
+            </div>
         </div>
     </section>
 </StyleRoot>
@@ -39,25 +69,41 @@ function AboutMyself(){
 // Displays about section
 function MyExperience(){
     return <StyleRoot>
-    <section id="about_me" className="  md:px-24 px-3 font-staatliches pt-1 min-h-screen overflow-x-hidden border border-white">
-        <div className=" relative top-9 xl:left-[22%] lg:left-[20%] left-10">
-            <article className=" text-gray-400 sm:text-xl xl:w-1/2 lg:w-4/6 md:w-5/6 w-9/12 text-justify" style={animationFadeIn._1000ms}>
-                Experience
-            </article>
+    <section id="about_me" className=" flex justify-center items-center font-staatliches min-h-screen overflow-x-hidden border border-white">
+        <div className=" relative  sm:w-[55%]">
+            <p className=" text-green-400 text-3xl">Work Experience</p>
+            <div className=" border border-white grid grid-cols-1 gap-y-3">
+                {   //loop through each companies and create a component
+                    list_companies.map((item) => {
+                        return <WorkExperienceHolder company_name={item.company_name} position={item.position}
+                        start_date={item.start_date} end_date={item.end_date}/>
+                    })
+                }
+            </div>
         </div>
     </section>
 </StyleRoot>
 }
 
-class ContentPage extends React.Component{
+function WorkExperienceHolder(props){
+    return <figure className=" flex border border-white">
+        <img src="" alt={props.company_name + "_logo"} className="border border-white w-36 h-36 mr-4"/>
+        <figcaption  className=" text-gray-400 sm:text-xl
+            text-justify border border-white">
+            <p className=" text-gray-200 text-3xl">{props.company_name}</p>
+            <p className=" text-xl">{props.position}</p>
+            <p className=" text-xl">{props.start_date} - {props.end_date}</p>
+        </figcaption>
+    </figure>
+}
 
-    
+class ContentPage extends React.Component{
 
     render(){
         return(
             <section>
                 <StyleRoot>
-                    <aside className=" absolute w-16 min-h-screen bottom-0 hidden sm:block">
+                    <aside className=" w-16 min-h-screen bottom-0 hidden sm:block sm:fixed">
                         <div className=' flex flex-col min-h-screen justify-center space-y-6 text-gray-200 items-center'>
                             {/* github */}
                             <a href="https://github.com/eSmooothie" target={'_blank'} 
